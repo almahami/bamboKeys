ddda23cf18178257086d3554598930d8ce4313c2
Mohammad Al Mahamid <almahamid98@gmail.com>
2021-12-03 19:43:07 +0100

    product

--- src/main/java/io/rest/BambooKeys/BambooKeysApplication.java
+++ src/main/java/io/rest/BambooKeys/BambooKeysApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+
 import io.rest.BambooKeys.service.InitializationTest;
 
 @SpringBootApplication
@@ -25,6 +26,7 @@ public class BambooKeysApplication {
 		return (evt)->{
 			
 			initializationTest.initUserDB();
+
 		};
 	}
 }
